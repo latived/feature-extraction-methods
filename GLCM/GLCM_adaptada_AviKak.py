bf9ffeb33d66b17ce2dc6b50b7ccc05caeebf512
vitalwarley <wvb@ic.ufal.br>
2018-03-10 09:39:02 -0300

    Melhorando ainda mais legibilidade

--- GLCM/GLCM_adaptada_AviKak.py
+++ GLCM/GLCM_adaptada_AviKak.py
@@ -5,19 +5,39 @@ import functools
 from PIL import Image
 import numpy # importar tudo msm é feio
 
-# contém a imagem original: 1.tiff
-img = Image.open("/home/lativ/Documents/UFAL/GioconDa/Dados/"
-    "ImagesDataset/ColonDB/CVC-ColonDB/CVC-ColonDB/7.tiff"
-    )
+print("Iniciando extração de atributos: GLCM")
 
-print("Caminho: {0}".format(img.filename))
-print("Dimensões: {0}".format(img.size))
+# A idéia do código é:
+# 1. receber uma imagem de videocolonoscopia
+# 2. converter para escala de cinza
+# 3. particionar em patches de (atualmente) 50x50
+# 4. calcular a matriz de co-ocorrência para cada patch
+# 5. extrair os atributos
+# 6. salvar os atributos desejados em um arquivo .csv [falta]
 
+### Etapa 1: contém a imagem original 
+# Talvez modifique para recebê-la como entrada
+img = Image.open("/home/lativ/Documents/"
+        "UFAL/GioconDa/Dados/"
+        "ImagesDataset/ColonDB/"
+        "CVC-ColonDB/CVC-ColonDB/7.tiff"
+        )
+
+print("Caminho da imagem: {0}".format(img.filename))
+print("Imagem carregada!")
+
+### Etapa 2: conversão para escala de cinza
 imgc = img.convert(mode="L") # RGB -> Cinza.
 # eu deveria usar o .quantize em vez do convert?
 
+print("Conversão para escala de cinza feita!")
+
+### Etapa 3: particionando imagem original em patches
+print("Preparando particionamento...")
+
 PATCH_SIZE      = 50        # é o tamanho do patch 
 GRAY_LEVELS     = 256       # imagem tem 24 níveis; requantizei para 8.
+
 # TO DO: usar outros valores para displacement, como no artigo [1 GLCM pra cada]
 # 0º  : [0, 1]
 # 45º : [-1,1]
@@ -40,6 +60,14 @@ for i in range(11):     # controla linhas
                         ((12 + PATCH_SIZE) + i * PATCH_SIZE)    # lower (upper + 50)
                     )
             )
+            
+            
+print("Particinamento completo!")
+print("=> Tamanho do patch usado: "
+        "{patch_size}x{patch_size}".format(patch_size=PATCH_SIZE))
+
+total_patches = len(patches) * len(patches[0]) # 11 * 10 para PATCH_SIZE=50
+print("=> Total de patches: {total}".format(total=total_patches))
 
 # Agora já tenhos os 110 patches, mas alguns são pretos.
 # Preciso descartar. Mas não eliminar-os-ei, por agora.
@@ -53,6 +81,9 @@ colmax  =   PATCH_SIZE - displacement[1] if displacement[1] else PATCH_SIZE - 1
 
 # Matriz 11x10, onde cada célula é uma GLCM
 patches_glcm = [[None for _ in range(10)] for _ in range(11)] 
+
+### Etapa 4: calcular matriz de co-ocorrência
+print("Calculando GLCM para cada patch.")
 
 # patch_i e patch_j localizam o patch que desejo na matriz acima.
 for patch_i in range(11):       # patch posição i
@@ -72,6 +103,11 @@ for patch_i in range(11):       # patch posição i
                 glcm[n][m] += 1
         # Uma glcm pronta, logo salvo
         patches_glcm[patch_i][patch_j] = glcm
+
+print("GLCMs construídas com sucesso.")
+
+### Etapa 5: extração dos atributos
+print("Preprando extração dos atributos...")
 
 
 # Para teste vou apenas selecionar uma glcm qualquer aqui
@@ -95,6 +131,8 @@ print("Localização do patch:"
 
 # Calcular atributos
 #   entropia, energia, constraste e homogeneidade
+
+print("Atributos: entropia, energia, contraste e homogeneidade.")
 
 entropy = energy = contrast = homogeneity = None     # por que não 0 em vez de None?
 # reduce(fun, seq[, initial]) -> value (isto é, reduz a matriz para a um valor)
@@ -129,7 +167,10 @@ for m in range(GRAY_LEVELS):
             homogeneity = prob / ( ( 1 + abs(m - n) ) * 1.0)
         homogeneity += prob / ( ( 1 + abs(m - n) ) * 1.0)
 if abs(entropy) < 0.0000001: entropy = 0.0
-print("\nTexture attributes: ")
+
+print("Atributos calculados para apenas uma GLCM: patches[{0}][{1}]".format(idx, jdx))
+
+print("\nAtributos de textura para matriz: ")
 print("     entropy: %f" % entropy)
 print("     energyy: %f" % energy)
 print("     contrast: %f" % contrast)
