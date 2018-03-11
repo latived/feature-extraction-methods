615cf02a7e2576b7c35b9a165e1800b947cb02c5
vitalwarley <wvb@ic.ufal.br>
2018-03-10 21:56:25 -0300

    GLCM e atributos para todos os patches da imagem salvos em arquivo .csv.

--- GLCM/GLCM_adaptada_AviKak.py
+++ GLCM/GLCM_adaptada_AviKak.py
@@ -6,7 +6,8 @@
 import math
 import functools
 from PIL import Image
-import numpy # importar tudo msm é feio
+import numpy 
+import csv 
 
 print("Iniciando extração de atributos: GLCM")
 
@@ -15,13 +16,12 @@ print("Iniciando extração de atributos: GLCM")
 # 2. converter para escala de cinza                     [feito, mas dúvidas]
 # 3. particionar em patches de (atualmente) 50x50       [feito]
 # 4. calcular matriz de co-ocorrência por patch         [1 de 4 direções][falta]
-# 5. extrair os atributos                               [compreender melhor]
-# 6. salvar os atributos desejados em um arquivo .csv   [falta]
+#        extrair os atributos                           [done]
+#        e salvar os atributos desejados 
+#           em um arquivo .csv                          [done]
 
 # Item 2: resultado ok, mas dúvida se é o melhor modo
 # Item 4: no artigo é obtida 1 matriz para cada distância (displacement)
-# Item 5: detalhes no local do código
-# Item 6: bom, não fiz.
 
 ### Etapa 1: contém a imagem original 
 # Talvez modifique para recebê-la como entrada
@@ -90,96 +90,63 @@ colmax  =   PATCH_SIZE - displacement[1] if displacement[1] else PATCH_SIZE - 1
 # Matriz 11x10, onde cada célula é uma GLCM
 patches_glcm = [[None for _ in range(10)] for _ in range(11)] 
 
-### Etapa 4: calcular matriz de co-ocorrência
+### Etapa 4: calcular matriz de co-ocorrência, extrair atributos e salvá-los
 print("Calculando GLCM para cada patch.")
+with open('attrs.csv', 'w', newline='') as attrs:
+    header = ['patch',
+            'entropia',
+            'energia',
+            'contraste',
+            'homogeneidade']
+    writer = csv.DictWriter(attrs, fieldnames=header)
+    writer.writeheader()
 
-# patch_i e patch_j localizam o patch que desejo na matriz acima.
-for patch_i in range(11):       # patch posição i
-    for patch_j in range(10):   # patch posição j
-        # inicializa matriz GLCM
-        glcm = [[0 for _ in range(GRAY_LEVELS)]
-                for _ in range(GRAY_LEVELS)]
-        # Image --> numpy.array
-        image = numpy.asarray(patches[patch_i][patch_j])
-        # Calculando GLCM para patches_glcm[patch_i][patch_j]
-        for i in range(rowmax):
-            for j in range(colmax):
-                # pega valores (m,n) no padrão setado e incrementa glcm[m][n] e glcm[n][m]
-                m, n = image[i][j], image[i + displacement[0]][j + displacement[1]]
-                # simétrica
-                glcm[m][n] += 1
-                glcm[n][m] += 1
-        # Uma glcm pronta, logo salvo
-        patches_glcm[patch_i][patch_j] = glcm
+    # patch_i e patch_j localizam o patch que desejo na matriz acima.
+    for patch_i in range(11):       # patch posição i
+        for patch_j in range(10):   # patch posição j
+            # inicializa matriz GLCM
+            glcm = [[0 for _ in range(GRAY_LEVELS)]
+                    for _ in range(GRAY_LEVELS)]
+            # Image --> numpy.array
+            image = numpy.asarray(patches[patch_i][patch_j])
+            # Calculando GLCM para patches_glcm[patch_i][patch_j]
+            for i in range(rowmax):
+                for j in range(colmax):
+                    # pega valores (m,n) no padrão setado e incrementa glcm[m][n] e glcm[n][m]
+                    m, n = image[i][j], image[i + displacement[0]][j + displacement[1]]
+                    # simétrica
+                    glcm[m][n] += 1
+                    glcm[n][m] += 1
+            # Uma glcm pronta, logo salvo
+            patches_glcm[patch_i][patch_j] = glcm
+            # já posso calcular os atributos aqui?
+            # mudando de None para 0
+            # não consigo ver algum problema por isso
+            entropy = energy = contrast = homogeneity = 0
+            normalizer = functools.reduce(lambda x, y: x + sum(y), glcm, 0)
+            for m in range(GRAY_LEVELS):
+                for n in range(GRAY_LEVELS):
+                    prob = (1.0 * glcm[m][n]) / normalizer
+                    if (prob >= 0.0001) and (prob <= 0.999):
+                        log_prob = math.log(prob, 2)
+                    if prob < 0.0001:
+                        log_prob = 0
+                    if prob > 0.999:
+                        log_prob = 0
+                    entropy += -1.0 * prob * log_prob
+                    energy += prob ** 2
+                    contrast += ((m - n) ** 2) * prob
+                    homogeneity += prob / ((1 + abs(m - n)) * 1.0)
+            if abs(entropy) < 0.0000001:
+                entropy = 0.0
+            # salvar os atributos em um arquivo
+            writer.writerow({'patch': 10 * (patch_i) + patch_j + 1,
+                'entropia': entropy,
+                'energia': energy,
+                'contraste': contrast,
+                'homogeneidade': homogeneity
+                })
 
-print("GLCMs construídas com sucesso.")
 
-### Etapa 5: extração dos atributos
-print("Preprando extração dos atributos...")
+print("GLCMs construídas, atributos extraídos e salvos com sucesso.")
 
-
-# Para teste vou apenas selecionar uma glcm qualquer aqui
-idx, jdx = 5, 4                 # indices do patch: linha, coluna
-glcm = patches_glcm[idx][jdx]   # seleciono a glcm
-
-# Formo o retângulo com (le,up) e (ri,lo)
-le = jdx * 50       # left
-up = 12 + idx * 50  # upper
-ri = 50 + jdx * 50  # right
-lo = 62 + idx * 50  # lower
-
-print("Localização do patch:"
-        " (left: {le}, upper: {up}, right: {ri}, lower: {lo})".format(
-            le = le, up = up, ri = ri, lo = lo
-            )
-        )
-
-
-# TO DO: salvar atributos em .csv? Vou usá-los com SVM, então...
-
-# Calcular atributos
-#   entropia, energia, constraste e homogeneidade
-
-print("Atributos: entropia, energia, contraste e homogeneidade.")
-
-entropy = energy = contrast = homogeneity = None     # por que não 0 em vez de None?
-# reduce(fun, seq[, initial]) -> value (isto é, reduz a matriz para a um valor)
-normalizer = functools.reduce(lambda x, y: x + sum(y), glcm, 0) # GLCM: [0,1]
-for m in range(GRAY_LEVELS):
-    for n in range(GRAY_LEVELS):
-        # Valores da GLCM como probabilidades
-        prob = (1.0 * glcm[m][n]) / normalizer
-        if (prob >= 0.0001) and (prob <= 0.999):
-            log_prob = math.log(prob,2)  
-        if prob < 0.0001:
-            log_prob = 0
-        if prob > 0.999:
-            log_prob = 0
-        # Por que a partir daqui os atributos são calculados apenas uma vez, no começo?
-        # entropy is None, então calcula.
-        # Aí passa pro próximo laço.
-        # O mesmo acontece para os outros.
-        if entropy is None:
-            entropy = -1.0 * prob * log_prob
-            continue   
-        entropy += -1.0 * prob * log_prob
-        if energy is None:
-            energy = prob ** 2
-            continue
-        energy += prob ** 2
-        if contrast is None:
-            contrast = ((m - n) ** 2) * prob
-            continue
-        contrast += ((m - n) ** 2) * prob
-        if homogeneity is None:
-            homogeneity = prob / ( ( 1 + abs(m - n) ) * 1.0)
-        homogeneity += prob / ( ( 1 + abs(m - n) ) * 1.0)
-if abs(entropy) < 0.0000001: entropy = 0.0
-
-print("Atributos calculados para apenas uma GLCM: patches[{0}][{1}]".format(idx, jdx))
-
-print("\nAtributos de textura para matriz: ")
-print("     entropy: %f" % entropy)
-print("     energyy: %f" % energy)
-print("     contrast: %f" % contrast)
-print("     homogeneity: %f" % homogeneity)
