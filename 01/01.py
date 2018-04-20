b14e55cbb57bcdf8335ebe0529d607d9a51f725c
vitalwarley <wvb@ic.ufal.br>
2018-04-19 23:48:24 -0300

    add 50x50 and 70x70 img patches

--- 01/01.py
+++ 01/01.py
@@ -6,13 +6,16 @@
 import os
 from skimage import io
 from skimage import color
+from skimage import util
 from skimage import img_as_ubyte
 from mahotas import features
 
 # TODO: automatizar para todas imagens que tenho
 img_path = "/home/lativ/Documents/UFAL/GioconDa/Dados/ImagesDataset/ColonDB/CVC-ColonDB/CVC-ColonDB/"
 filename = os.path.join(img_path, "1.tiff")
-colon = io.imread(filename) # TODO: usar mahotas.imread?
+# TODO: usar mahotas.imread?
+# TODO: usar as_grey parameter?
+colon = io.imread(filename) 
 
 # INFO: colon_gray está em float64, logo converto pra ubyte (inteiro 8 bits)
 # INFO: Precisão pode ser perdida.
@@ -20,7 +23,30 @@ colon_gray = img_as_ubyte(color.rgb2gray(colon)) # TODO: há outra melhor forma?
 # TODO: ver mahotas.strech(img)
 
 # TODO: cortar cada imagem em patches 50x50 e 70x70
-# algo assim: colon_gray[50:100, 50:100]
+
+# para 50x50 temos 10x11 patches
+# para 70x70 temos 7x8 patches
+
+# colon_gray.shape -> (500, 574)
+# colon_gray[0:50, 12:62] ... [450:500, 512:562]
+# colon_gray[5:75, 7:77] .. [424:495, 497:567]
+
+# TODO: encontrar uma maneira automática de construir esses patches
+patches_50 = [
+        [colon_gray[
+            (line * 50):(50 + line * 50),
+            (12 + col * 50):(12 + 50) + col * 50
+            ] 
+            for col in range(11)] 
+                for line in range(10)]
+
+patches_70 = [
+        [colon_gray[
+            (5 + line * 70):(5 + 70) + line * 70),
+            (7 + col * 70):(7 + 70) + col * 70
+            ]
+            for col in range(8)] 
+        for line in range(7)]
 
 # TODO: Haralick features
 #
